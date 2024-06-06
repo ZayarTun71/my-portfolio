2032d0a81ef6969d4bcecc06803142bc8f3ba087
ZayarTun71 <zayarjob@gmail.com>
2024-06-06 22:58:28 +0630

    add projects details

--- js/main.js
+++ js/main.js
@@ -93,66 +93,222 @@
 
   ///#Project
 
-  //project dynamic
   const portfolioItems = [
     {
       imgSrc: "img/ticket-managemnet.png",
       title: "Ticket Management",
       filter: "filter-2",
       delay: "0.0s",
+      languages: [
+        {
+          name: "Php",
+          color: "#7B7FB6",
+        },
+        {
+          name: "Laravel",
+          color: "#FE5443",
+        },
+        {
+          name: "Reactjs",
+          color: "#1DCEFF",
+        },
+        {
+          name: "Typescript",
+          color: "#387CC8",
+        },
+        {
+          name: "Mysql",
+          color: "#4A7DA3",
+        },
+      ],
+      description:"A Ticket Management System (TMS) is a software solution designed to help organizations manage and track customer support requests, issues, and inquiries efficiently. It serves as a centralized platform where support teams can organize, prioritize, and resolve tickets (requests) raised by customers or users."
     },
     {
       imgSrc: "img/interview-management.png",
       title: "Interview Management",
       filter: "filter-1",
       delay: "0.2s",
+      languages: [
+        {
+          name: "Php",
+          color: "#7B7FB6",
+        },
+        {
+          name: "Laravel",
+          color: "#FE5443",
+        },
+        {
+          name: "Reactjs",
+          color: "#1DCEFF",
+        },
+        {
+          name: "Typescript",
+          color: "#387CC8",
+        },
+        {
+          name: "Mysql",
+          color: "#4A7DA3",
+        },
+      ],
+      description:"An Interview Management System (IMS) is a comprehensive tool that facilitates the scheduling, coordination, and management of job interviews. It aims to enhance the efficiency and effectiveness of the recruitment process by automating various tasks, ensuring smooth communication between all stakeholders, and providing a centralized platform for managing candidate information and interview logistics.",
+
     },
     {
       imgSrc: "img/zenshou.png",
       title: "全商Webシステム",
       filter: "filter-3",
       delay: "0.4s",
+      languages: [
+        {
+          name: "Coldfusion",
+          color: "#07295D",
+        },
+        {
+          name: "Microsoft Sql Server",
+          color: "#C9CDCC",
+        },
+      ],
+      description:"The Zensho Web System is an education-related web system used in Japanese commercial high schools and vocational schools. This system is designed to streamline school administrative tasks and student learning support. General functions include the following:",
     },
     {
       imgSrc: "img/kanji-cards.jpg",
       title: "Kanji Flash Card",
       filter: "filter-3",
       delay: "0.8s",
+      languages: [
+        {
+          name: "Nodejs",
+          color: "#5AA14B",
+        },
+        {
+          name: "Expressjs",
+          color: "#383838",
+        },
+        {
+          name: "Sequelize",
+          color: "#1EB1EF",
+        },
+        {
+          name: "Postgresql",
+          color: "#3A6C94",
+        },
+      ],
+      description:
+        "A Kanji Flash Card System is a learning aid used to memorize and reinforce Japanese kanji characters, which are logographic characters used in the Japanese writing system. It provides a structured approach to learning kanji by presenting characters on flashcards, along with their meanings, readings (pronunciations), and example usage.",
     },
     {
       imgSrc: "img/sarmal-warmal.png",
       title: "Sar Mal War Mal",
       filter: "filter-2",
       delay: "1s",
+      languages: [
+        {
+          name: "Php",
+          color: "#7B7FB6",
+        },
+        {
+          name: "Laravel",
+          color: "#FE5443",
+        },
+        {
+          name: "Reactjs",
+          color: "#1DCEFF",
+        },
+        {
+          name: "Mysql",
+          color: "#4A7DA3",
+        },
+      ],
+      description:
+        "A Healthy Foods Selling System is a platform that enables businesses to sell and promote a variety of healthy food items, catering to individuals looking to make healthier dietary choices. It may encompass various features to support online sales, inventory management, customer engagement, and nutritional information dissemination.",
     },
     {
       imgSrc: "img/hospital-appointment.jfif",
-      title: "Hospital appointment",
+      title: "Hospital Appointment",
       filter: "filter-3",
       delay: "0.6s",
+      languages: [
+        {
+          name: "Nodejs",
+          color: "#5AA14B",
+        },
+        {
+          name: "Expressjs",
+          color: "#383838",
+        },
+        {
+          name: "Sequelize",
+          color: "#1EB1EF",
+        },
+        {
+          name: "Postgresql",
+          color: "#3A6C94",
+        },
+      ],
+      description:
+        "A Hospital Appointment System serves as a centralized platform for managing patient appointments, enabling healthcare providers to efficiently allocate resources, reduce wait times, and improve patient satisfaction. It facilitates seamless communication between patients, healthcare staff, and administrative personnel throughout the appointment scheduling process.",
     },
   ];
 
   const portfolioContainer = document.getElementById("portfolioContainer");
+  const portfolioModal = document.getElementById("portfolioModal");
+  const modalImage = document.getElementById("modalImage");
+  const modalTitle = document.getElementById("portfolioModalLabel");
+  const modalDescription = document.getElementById("modalDescription");
+  const modalLanguages = document.getElementById("modalLanguages");
 
-  portfolioItems.forEach((item) => {
+  portfolioItems.forEach((item, index) => {
     const portfolioItem = document.createElement("div");
     portfolioItem.className = `col-lg-4 col-md-6 col-sm-12 portfolio-item ${item.filter} wow fadeInUp`;
     portfolioItem.setAttribute("data-wow-delay", item.delay);
 
     portfolioItem.innerHTML = `
-            <div class="portfolio-wrap">
-                <div class="portfolio-img">
-                    <img src="${item.imgSrc}" alt="Image">
-                </div>
-                <div class="portfolio-text">
-                    <h3>${item.title}</h3>
-                    <a class="btn" href="${item.imgSrc}" data-lightbox="portfolio">+</a>
-                </div>
-            </div>
-        `;
+  <div class="portfolio-wrap">
+      <div class="portfolio-img">
+          <img src="${item.imgSrc}" alt="${item.title}">
+      </div>
+      <div class="portfolio-text">
+          <h3>${item.title}</h3>
+          <a class="btn" href="#" data-toggle="modal" data-target="#portfolioModal" 
+             data-title="${item.title}" data-img="${item.imgSrc}" 
+             data-description="${item.description}" 
+             data-languages='${JSON.stringify(item.languages)
+               .replace(/'/g, "&apos;")
+               .replace(/"/g, "&quot;")}'>+</a>
+      </div>
+  </div>
+`;
 
     portfolioContainer.appendChild(portfolioItem);
+  });
+
+  $("#portfolioModal").on("show.bs.modal", function (event) {
+    const button = $(event.relatedTarget); // Button that triggered the modal
+    const title = button.data("title"); // Extract info from data-* attributes
+    const imgSrc = button.data("img");
+    const description = button.data("description");
+    const languages = JSON.parse(
+      button
+        .attr("data-languages")
+        .replace(/&quot;/g, '"')
+        .replace(/&apos;/g, "'")
+    );
+
+    const modal = $(this);
+    modal.find(".modal-title").text(title);
+    modal.find(".modal-body img").attr("src", imgSrc);
+    modal.find(".modal-body #modalDescription").text(description);
+
+    const languagesList = modal.find(".modal-body #modalLanguages");
+    languagesList.empty();
+    languages.forEach((language) => {
+      const colorStyle = language.color
+        ? `style="background-color: ${language.color};"`
+        : "";
+      languagesList.append(
+        `<li class="list-inline-item badge badge-primary" ${colorStyle}>${language.name}</li>`
+      );
+    });
   });
 
   //project filter
@@ -387,7 +543,7 @@
     const teamItem = document.createElement("div");
     teamItem.className = `col-lg-6 wow fadeInUp`;
     teamItem.setAttribute("data-wow-delay", member.delay);
-  
+
     teamItem.innerHTML = `
       <div class="team-item">
           <div class="team-img">
@@ -406,13 +562,13 @@
     `;
     return teamItem;
   }
-  
-//   Loop through the team members and create rows of 4 members each
+
+  //   Loop through the team members and create rows of 4 members each
   for (let i = 0; i < teamMembers.length; i += 4) {
     const row = document.createElement("div");
     row.className = "row";
     teamContainer.appendChild(row);
-  
+
     // Loop through the next four members or less if there are fewer than four remaining
     for (let j = i; j < i + 4 && j < teamMembers.length; j++) {
       const member = teamMembers[j];
@@ -421,50 +577,22 @@
     }
   }
 
-    if($(window).width() > 760){
-        teamContainer.className = ` owl-carousel`;
-       
-        $('#teamContainer').owlCarousel({
-            center: true,
-            autoplay: true,
-            loop: true,
-            margin:0.1,
-            animateOut: 'fadeOut',
-            responsive: {
-              0: {
-                items: 1,
-              },
-            },
-        })
-    }
+  if ($(window).width() > 760) {
+    teamContainer.className = ` owl-carousel`;
 
-
-//   const teamContainer = document.querySelector('#teamContainer');
-
-//   teamMembers.forEach(member => {
-//       const teamItem = document.createElement('div');
-//       teamItem.className = `col-lg-6 wow fadeInUp`;
-//       teamItem.setAttribute('data-wow-delay', member.delay);
-
-//       teamItem.innerHTML = `
-//           <div class="team-item">
-//               <div class="team-img">
-//                   <img src="${member.image}" alt="${member.name}">
-//               </div>
-//               <div class="team-text">
-//                   <h2>${member.name}</h2>
-//                   <h4>${member.role}</h4>
-//                   <p></p>
-//                   <div class="team-social">
-//                       <a class="btn" href="${member.facebook}"><i class="fab fa-facebook-f"></i></a>
-//                       <a class="btn" href="${member.linkedin}"><i class="fab fa-linkedin-in"></i></a>
-//                   </div>
-//               </div>
-//           </div>
-//       `;
-
-//       teamContainer.appendChild(teamItem);
-//   });
+    $("#teamContainer").owlCarousel({
+      center: true,
+      autoplay: true,
+      loop: true,
+      margin: 0.1,
+      animateOut: "fadeOut",
+      responsive: {
+        0: {
+          items: 1,
+        },
+      },
+    });
+  }
 
   ///Skills
 
@@ -485,12 +613,12 @@
     { img: "img/sequelize.png", name: "Sequelize" },
     { img: "img/reactjs.png", name: "React.js" },
     { img: "img/typescript.png", name: "Typescript" },
-    ,
     { img: "img/java.png", name: "Java" },
     { img: "img/coldfusion.png", name: "Coldfusion" },
     { img: "img/mysql.png", name: "Mysql" },
     { img: "img/postgres.png", name: "PostgreSql" },
     { img: "img/git.png", name: "Git" },
+    { img: "img/Asure.png", name: "Asure" },
     { img: "img/docker.png", name: "Docker" },
     { img: "img/postman.png", name: "Postman" },
   ];
